719c1434f8762420ecfd9d1133399cff71a1d0aa
Iswar-Ch-Rana <ranabitu227@gmail.com>
2023-05-13 21:11:01 +0530

    sequences and subset problem leetcode

--- tut37_subset_subsiquence.cpp
+++ tut37_subset_subsiquence.cpp
@@ -1,8 +1,59 @@
-#include <iostream>
-using namespace std;
 
-int main()
+// LeetCode 78. Subsets
+
+class Solution
+{
+public:
+    void solve(vector<int> nums, int index, vector<int> output, vector<vector<int>> &ans)
+    {
+        // Base case
+        if (index >= nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+        // Exclude
+        solve(nums, index + 1, output, ans);
+        // Include
+        int element = nums[index];
+        output.push_back(element);
+        solve(nums, index + 1, output, ans);
+    }
+
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, index, output, ans);
+        return ans;
+    }
+};
+
+// Subsequences of String CodeSTudio
+
+void solve(vector<string> &ans, string str, string output, int i)
+{
+    // base case
+    if (i >= str.length())
+    {
+        if (output.length() > 0)
+            ans.push_back(output);
+        return;
+    }
+
+    // exclude
+    solve(ans, str, output, i + 1);
+    // include
+    output.push_back(str[i]);
+    solve(ans, str, output, i + 1);
+}
+
+vector<string> subsequences(string str)
 {
 
-    return 0;
+    vector<string> ans;
+    string output = "";
+    solve(ans, str, output, 0);
+    return ans;
 }
